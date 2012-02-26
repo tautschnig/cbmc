1fd7cdbd1c4657ffec0f3736313b45cd5416fb86
Michael Tautschnig <mt@debian.org>
2012-02-26 21:44:01 +0000

    Make convert_bv return a reference instead of writing to parameter

--- src/solvers/flattening/boolbv_reduction.cpp
+++ src/solvers/flattening/boolbv_reduction.cpp
@@ -24,7 +24,7 @@ literalt boolbvt::convert_reduction(const unary_exprt &expr)
 {
   const bvt &op_bv=convert_bv(expr.op());
 
-  if(op_bv.size()<1)
+  if(op_bv.empty())
     throw "reduction operators take one non-empty operand";
 
   enum { O_OR, O_AND, O_XOR } op;
