edc3a04550a58584c1705b4e244eab032ebe7ec4
Michael Tautschnig <mt@debian.org>
2013-10-22 13:18:46 +0100

    Visual Studio vs. Cygwin vs. MinGW: win32 is a messy platform
    - Visual Studio is the native platform on Windows
    - --gcc --win32 now works for MinGW
    - --linux-i386 ought to work for Cygwin

--- src/ansi-c/ansi_c_internal_additions.cpp
+++ src/ansi-c/ansi_c_internal_additions.cpp
@@ -290,7 +290,8 @@ void ansi_c_internal_additions(std::string &code)
   // this is Visual C/C++ only
   if(config.ansi_c.os==configt::ansi_ct::ost::OS_WIN)
     code+="int __noop();\n"
-          "int __assume(int);\n";
+          "int __assume(int);\n"
+          "typedef void ** __builtin_va_list;\n";
     
   // ARM stuff
   if(config.ansi_c.mode==configt::ansi_ct::flavourt::MODE_ARM_C_CPP)
