1887ea6e62df43439739a6b52ada6134c5c6f37d
Siqing Tang <jtang707@gmail.com>
2014-07-20 04:04:20 +0100

    added missings

---
+++ src/goto-programs/interpreter_command.cpp
@@ -0,0 +1,196 @@
+/*******************************************************************\
+
+Module: Interpreter command for GOTO Instrument Programs
+
+Author: Siqing Tang, jtang707@gmail.com
+
+\*******************************************************************/
+
+#include <cctype>
+#include <cstdio>
+#include <iostream>
+#include <util/std_types.h>
+
+#include "interpreter_command.h"
+
+void interpretert_command::ask()
+{
+	#define BUFSIZE 100
+
+	char command[BUFSIZE];  
+
+	std::cout << std::endl << "Command (q to quit; h for help): ";
+	if (fgets(command, BUFSIZE - 1, stdin) == NULL)
+  {
+		tokens.clear();
+		options.clear();
+  }
+	else
+	{
+		parse(command);
+	}
+}
+
+void interpretert_command::print_help() const
+{
+  std::cout << "\tq - quit" << std::endl
+            << "\th - help" << std::endl
+            << "\tr - restart" << std::endl;
+  
+  //if (completed) return;
+
+  std::cout << "\tm - run until the main" << std::endl
+            << "\tn - next line [ENTER]" << std::endl
+            << "\tsi - step into" << std::endl
+            << "\tso - step out" << std::endl;
+}
+
+bool interpretert_command::is_quit() const
+{
+	return !tokens.empty() && (
+		tokens.front() == "quit" ||
+		tokens.front() == "q");
+}
+
+bool interpretert_command::is_help() const
+{
+	return !tokens.empty() && (
+		tokens.front() == "help" ||
+		tokens.front() == "h"    ||
+		tokens.front() == "?");
+}
+
+bool interpretert_command::is_restart() const
+{
+	return !tokens.empty() && (
+		tokens.front() == "restart" ||
+		tokens.front() == "r");
+}
+
+bool interpretert_command::is_run_until_main() const
+{
+	return !tokens.empty() && (
+		tokens.front() == "main" ||
+		tokens.front() == "m");
+}
+
+bool interpretert_command::is_next_line() const
+{
+	return tokens.empty() ||
+		tokens.front() == "next" ||
+		tokens.front() == "n";
+}
+
+bool interpretert_command::is_step_into() const
+{
+	return !tokens.empty() && (
+		(tokens.size() == 1 && tokens.front() == "si") ||
+		(tokens.size() == 2 && 
+		 tokens.front() == "step" && 
+		 tokens[1] == "into"));
+}
+
+bool interpretert_command::is_step_over() const
+{
+	return !tokens.empty() && (
+		(tokens.size() == 1 && tokens.front() == "so") ||
+		(tokens.size() == 2 && 
+		 tokens.front() == "step" && 
+		 tokens[1] == "over"));
+}
+
+bool interpretert_command::is_print() const
+{
+	return !tokens.empty() &&
+		(tokens.front() == "print" ||
+		 tokens.front() == "p");
+}
+
+bool interpretert_command::is_list() const
+{
+	return !tokens.empty() &&
+		(tokens.front() == "list" ||
+		 tokens.front() == "l");
+}
+
+void interpretert_command::get_parameters(std::vector<std::string> &dest) const
+{
+	dest.clear();
+
+	unsigned skip = 1;
+
+	// more logics here. for now just skip 1
+	// for example
+	// if (is_print())
+	//    skip = 1
+	// else if (...)
+	//
+	if (skip >= tokens.size()) return;
+
+	dest.resize(tokens.size() - skip);
+	for(unsigned i = skip; i < tokens.size(); i++)
+	{
+		dest.push_back(tokens[i]);
+	}
+}
+
+void interpretert_command::get_options(std::vector<std::string> &dest) const
+{
+	dest.clear();
+	for(unsigned i = 0; i < options.size(); i++)
+	{
+		dest.push_back(options[i]);
+	}
+}
+
+void interpretert_command::parse(const char* cmdline)
+{
+  #define SPACE ' '
+
+  tokens.clear();
+	options.clear();
+
+  std::string line(cmdline);
+
+  size_t pos;
+
+  // remove LF if any
+  pos = line.find_last_of('\n');
+  if (pos != std::string::npos)
+  {
+    line.resize(line.size() - 1);
+  }
+
+  // remove CR if any
+  pos = line.find_last_of('\r');
+  if (pos != std::string::npos)
+  {
+    line.resize(line.size() - 1);
+  }
+
+  pos = line.find_first_not_of(SPACE);
+  while (pos != std::string::npos)
+  {
+    size_t sp_pos = line.find_first_of(SPACE, pos);
+		std::string s;
+    if (sp_pos == std::string::npos)
+    {
+      s = line.substr(pos);
+      pos = sp_pos;
+    }
+    else
+    {
+      s = line.substr(pos, sp_pos - pos);
+      pos = line.find_first_not_of(SPACE, sp_pos + 1);
+    }
+
+		if (s.length() >= 2 && s.substr(0, 2) == "--")
+		{
+			options.push_back(s);
+		}
+		else 
+		{
+			tokens.push_back(s);
+		}
+  }
+}
---
+++ src/goto-programs/interpreter_command.h
@@ -0,0 +1,48 @@
+/*******************************************************************\
+
+   Class: interpretert_command
+
+ Purpose: interpreter command parser GOTO interpreter
+
+\*******************************************************************/
+
+#ifndef GOTO_INTERPRET_COMMAND_H
+#define GOTO_INTERPRET_COMMAND_H
+
+#include <vector>
+
+class interpretert_command
+{
+
+public:
+  interpretert_command()
+	{
+  }
+  
+  void ask();
+
+	void print_help() const;
+
+	bool is_quit() const;
+	bool is_help() const;
+	bool is_restart() const;
+	bool is_run_until_main() const;
+	bool is_next_line() const;
+	bool is_step_into() const;
+	bool is_step_over() const;
+	bool is_print() const;
+	bool is_list() const;
+
+  void get_parameters(std::vector<std::string> &dest) const;
+  void get_options(std::vector<std::string> &dest) const;
+protected:
+	std::vector<std::string> tokens; //all but options (see below)
+
+	// --blah or --blah=blah is an option.
+	// all options are stored in options vector
+	std::vector<std::string> options;
+
+	void parse(const char* cmdline);
+};
+
+#endif /* GOTO_INTERPRET_COMMAND_H */
