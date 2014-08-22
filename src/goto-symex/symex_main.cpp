8ddbde2de1abb867420d461cec15cccd441c2ebf
Michael Tautschnig <mt@debian.org>
2014-08-22 11:44:33 +0200

    Fix field sensitivity for function calls

--- src/goto-symex/symex_main.cpp
+++ src/goto-symex/symex_main.cpp
@@ -546,10 +546,6 @@ void goto_symext::symex_step(
         clean_expr(*it, state, false);
     
       symex_function_call(goto_functions, state, deref_code);
-
-      if(deref_code.lhs().is_not_nil())
-        state.field_sensitivity.field_assignments(
-          ns, state, target, deref_code.lhs());
     }
     else
       state.source.pc++;
