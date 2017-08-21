58ed144903dd14831e36e15fb08a8e8ca2b82e90
Michael Tautschnig <tautschn@amazon.com>
2017-08-21 12:05:28 +0100

    unclear change 2 -- still 12 failures

--- src/cpp/cpp_name.cpp
+++ src/cpp/cpp_name.cpp
@@ -32,7 +32,11 @@ irep_idt cpp_namet::get_base_name() const
 
   if(sub[base].id()==ID_name)
     return sub[base].get(ID_identifier);
+  else if(base+1<sub.size() && sub[base].id()==ID_operator &&
+           sub[base+1].id()==ID_name)
+    return "operator"+sub[base+1].get_string(ID_identifier);
   else if(base+1<sub.size() && sub[base].id()==ID_operator)
+    //TODO: the name produced for cast operators is not very meaningful
     return "operator"+sub[base+1].id_string();
   else if(base+1<sub.size() && sub[base].id()=="~" && sub[base+1].id()==ID_name)
     return "~"+sub[base+1].get_string(ID_identifier);
