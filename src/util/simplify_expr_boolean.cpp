45673a4355c457bc9bc2530bd6d1866db2161d7f
Michael Tautschnig <tautschn@amazon.com>
2019-02-13 14:49:44 +0000

    Perform simplification of not_exprt as preorder step
    Each of the possible simplifications has an impact on its operands,
    which in turn mostly needs to be simplified. Instead of doing this again
    (as the operand has already been simplified), do most of the work as a
    preorder step so that only the modified operand is subject to
    simplification.

--- src/util/simplify_expr_boolean.cpp
+++ src/util/simplify_expr_boolean.cpp
@@ -234,3 +234,82 @@ bool simplify_exprt::simplify_not(exprt &expr)
 
   return true;
 }
+
+bool simplify_exprt::simplify_not_preorder(exprt &expr)
+{
+  if(expr.operands().size() != 1)
+    return true;
+
+  exprt &op = expr.op0();
+
+  if(expr.type().id() != ID_bool || op.type().id() != ID_bool)
+    return true;
+
+  if(op.id() == ID_not) // (not not a) == a
+  {
+    if(op.operands().size() == 1)
+    {
+      exprt tmp;
+      tmp.swap(op.op0());
+      expr.swap(tmp);
+      simplify_rec(expr);
+      return false;
+    }
+  }
+  else if(op.is_false())
+  {
+    expr = true_exprt();
+    return false;
+  }
+  else if(op.is_true())
+  {
+    expr = false_exprt();
+    return false;
+  }
+  else if(op.id() == ID_and || op.id() == ID_or)
+  {
+    exprt tmp;
+    tmp.swap(op);
+    expr.swap(tmp);
+
+    Forall_operands(it, expr)
+    {
+      *it = boolean_negate(*it);
+    }
+
+    expr.id(expr.id() == ID_and ? ID_or : ID_and);
+
+    simplify_rec(expr);
+
+    return false;
+  }
+  else if(op.id() == ID_notequal) // !(a!=b) <-> a==b
+  {
+    exprt tmp;
+    tmp.swap(op);
+    expr.swap(tmp);
+    expr.id(ID_equal);
+    simplify_rec(expr);
+    return false;
+  }
+  else if(op.id() == ID_exists) // !(exists: a) <-> forall: not a
+  {
+    auto const &op_as_exists = to_exists_expr(op);
+    forall_exprt rewritten_op(
+      op_as_exists.symbol(), not_exprt(op_as_exists.where()));
+    expr = rewritten_op;
+    simplify_rec(expr);
+    return false;
+  }
+  else if(op.id() == ID_forall) // !(forall: a) <-> exists: not a
+  {
+    auto const &op_as_forall = to_forall_expr(op);
+    exists_exprt rewritten_op(
+      op_as_forall.symbol(), not_exprt(op_as_forall.where()));
+    expr = rewritten_op;
+    simplify_rec(expr);
+    return false;
+  }
+
+  return simplify_rec(op);
+}
