20ea09a6878dbc331abc06dede68f72eaacf7671
Michael Tautschnig <tautschn@amazon.com>
2019-02-27 08:53:59 +0000

    C library: Constrain the return value of fileno and test it
    The earlier commit made sure it returns 0/1/2 for stdin/stdout/stderr.

--- regression/cbmc-library/fileno-01/main.c
+++ regression/cbmc-library/fileno-01/main.c
@@ -3,7 +3,12 @@
 
 int main()
 {
-  fileno();
-  assert(0);
+  assert(fileno(stdin) == 0);
+  assert(fileno(stdout) == 1);
+  assert(fileno(stderr) == 2);
+
+  FILE *some_file;
+  assert(fileno(some_file) >= -1);
+
   return 0;
 }
