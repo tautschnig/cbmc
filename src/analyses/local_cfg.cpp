832498f871cbe9eed3cbb858d3ba808d6d78cd43
martin <martin@raphael>
2019-03-04 11:46:08 +0000

    Annotate missed cases in the transform functions of various domains
    There are a number of reasons why instruction types were left out of
    these case statements :
    
    1. Ignoring this instruction is generally a valid overapproximation.
    2. Ignoring this instruction is a valid overapproximation for this
       domain.
    3. The instruction is assumed to not be present due to preceeding
       passes.
    4. The instruction should never appear in any valid goto program.
    5. The instruction is newer than the analysis code and was forgotten.
    
    This patch tries to correctly document which of these apply.

--- src/analyses/local_cfg.cpp
+++ src/analyses/local_cfg.cpp
@@ -86,10 +86,14 @@ void local_cfgt::build(const goto_programt &goto_program)
     case DECL:
     case DEAD:
     case ASSIGN:
-    case INCOMPLETE_GOTO:
-    case NO_INSTRUCTION_TYPE:
       node.successors.push_back(loc_nr+1);
       break;
+
+    case INCOMPLETE_GOTO:
+    case NO_INSTRUCTION_TYPE:
+      DATA_INVARIANT(false, "Only complete instructions can be analysed");
+      break;
+
     }
   }
 }
