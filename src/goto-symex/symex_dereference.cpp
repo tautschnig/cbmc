d1d5cb23f4dbd5761fe4183371769e24b50f3675
Michael Tautschnig <tautschn@amazon.com>
2019-03-11 13:43:47 +0000

    Field-sensitive level-2 SSA renaming
    Struct member and array index expressions are replaced by new SSA
    symbols such that updates to individual fields of structs or arrays do
    not affect the SSA index of the remaining object. This enables more
    fine-grained constant propagation, which should be particularly
    beneficial to Java (where structs/classes prevail).

--- src/goto-symex/symex_dereference.cpp
+++ src/goto-symex/symex_dereference.cpp
@@ -357,6 +357,7 @@ void goto_symext::dereference(exprt &expr, statet &state, bool write)
   // symbols whose address is taken.
   PRECONDITION(!state.call_stack().empty());
   exprt l1_expr = state.rename<L1>(expr, ns).get();
+  state.field_sensitivity.apply(ns, l1_expr, write);
 
   // start the recursion!
   dereference_rec(l1_expr, state, write);
@@ -389,4 +390,6 @@ void goto_symext::dereference(exprt &expr, statet &state, bool write)
       !has_subexpr(expr, ID_dereference),
       "simplify re-introduced dereferencing");
   }
+
+  state.field_sensitivity.apply(ns, expr, write);
 }
