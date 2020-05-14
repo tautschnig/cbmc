38b492abea56d72fddf26b187dd9588ef9e8f54d
Mark R Tuttle <mrtuttle@amazon.com>
2020-05-14 22:59:50 +0000

    Install into cbmc-latest

--- .github/workflows/vs2019/build-installer.bat
+++ .github/workflows/vs2019/build-installer.bat
@@ -1,17 +1,19 @@
-@REM Install bison and flex
+@REM Build installer
 
 echo on
 
 set PKG=%1
+set NAME=cbmc-latest
+set SCRIPTDIR=.github\workflows\vs2019
 
 echo "Set up PATH to find wix"
 PATH=%PATH%;"C:\Program Files (x86)\WiX Toolset v3.11\bin"
 
 echo "Build installer: run candle"
-candle -o cbmc.wixobj -arch x64 .github\workflows\vs2019\cbmc.wxs
+candle -o %NAME%.wixobj -arch x64 %SCRIPTDIR%\%NAME%.wxs
 
 echo "Build installer: run light"
-light -o %PKG% -ext WixUIExtension cbmc.wixobj
+light -o %PKG% -ext WixUIExtension %NAME%.wixobj
 
 echo "Package is %PKG%"
 echo %PKG%
