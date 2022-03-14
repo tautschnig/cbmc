79232466f9f47db13fdd734731fb3341f951b0ad
Michael Tautschnig <tautschn@amazon.com>
2022-03-14 12:29:33 +0000

    Make find_symbols implementations uniform
    Each function used a different approach, with different potential
    omissions of symbols. Instead, try to use the most efficient and
    complete approaches, and re-use code by making some functions just thin
    wrappers.

--- src/util/find_symbols.cpp
+++ src/util/find_symbols.cpp
@@ -50,67 +50,93 @@ bool has_symbol(
   return has_symbol(src, symbols, true, true);
 }
 
+static void find_symbols(const typet &src, std::set<symbol_exprt> &dest)
+{
+  if(src.has_subtype())
+    find_symbols(to_type_with_subtype(src).subtype(), dest);
+
+  for(const typet &subtype : to_type_with_subtypes(src).subtypes())
+    find_symbols(subtype, dest);
+
+  if(src.id() == ID_struct || src.id() == ID_union)
+  {
+    const struct_union_typet &struct_union_type = to_struct_union_type(src);
+
+    for(const auto &c : struct_union_type.components())
+      find_symbols(c, dest);
+  }
+  else if(src.id() == ID_code)
+  {
+    const code_typet &code_type = to_code_type(src);
+    find_symbols(code_type.return_type(), dest);
+
+    for(const auto &p : code_type.parameters())
+    {
+      find_symbols(p, dest);
+    }
+  }
+  else if(src.id() == ID_array)
+  {
+    // do the size -- the subtype is already done
+    find_symbols(to_array_type(src).size(), dest);
+  }
+}
+
 void find_symbols(
   const exprt &src,
   std::set<symbol_exprt> &dest)
 {
   src.visit_pre([&dest](const exprt &e) {
+    find_symbols(e.type(), dest);
+
     if(e.id() == ID_symbol)
       dest.insert(to_symbol_expr(e));
-  });
-}
 
-std::set<symbol_exprt> find_symbols(const exprt &src)
-{
-  return make_range(src.depth_begin(), src.depth_end())
-    .filter([](const exprt &e) { return e.id() == ID_symbol; })
-    .map([](const exprt &e) { return to_symbol_expr(e); });
-}
+    const irept &c_sizeof_type = e.find(ID_C_c_sizeof_type);
 
-std::unordered_set<irep_idt> find_symbol_identifiers(const exprt &src)
-{
-  std::unordered_set<irep_idt> result;
-  src.visit_pre([&](const exprt &e) {
-    if(e.id() == ID_symbol)
-      result.insert(to_symbol_expr(e).get_identifier());
+    if(c_sizeof_type.is_not_nil())
+      find_symbols(static_cast<const typet &>(c_sizeof_type), dest);
+
+    const irept &va_arg_type = e.find(ID_C_va_arg_type);
+
+    if(va_arg_type.is_not_nil())
+      find_symbols(static_cast<const typet &>(va_arg_type), dest);
   });
-  return result;
 }
 
 void find_symbols(kindt kind, const typet &src, find_symbols_sett &dest);
 
 void find_symbols(kindt kind, const exprt &src, find_symbols_sett &dest)
 {
-  forall_operands(it, src)
-    find_symbols(kind, *it, dest);
+  src.visit_pre([&dest, kind](const exprt &e) {
+    find_symbols(kind, e.type(), dest);
 
-  find_symbols(kind, src.type(), dest);
+    if(
+      kind == kindt::F_ALL || kind == kindt::F_EXPR_CURRENT ||
+      kind == kindt::F_EXPR_BOTH)
+    {
+      if(e.id() == ID_symbol)
+        dest.insert(to_symbol_expr(e).get_identifier());
+    }
 
-  if(
-    kind == kindt::F_ALL || kind == kindt::F_EXPR_CURRENT ||
-    kind == kindt::F_EXPR_BOTH)
-  {
-    if(src.id() == ID_symbol)
-      dest.insert(to_symbol_expr(src).get_identifier());
-  }
+    if(
+      kind == kindt::F_ALL || kind == kindt::F_EXPR_NEXT ||
+      kind == kindt::F_EXPR_BOTH)
+    {
+      if(e.id() == ID_next_symbol)
+        dest.insert(e.get(ID_identifier));
+    }
 
-  if(
-    kind == kindt::F_ALL || kind == kindt::F_EXPR_NEXT ||
-    kind == kindt::F_EXPR_BOTH)
-  {
-    if(src.id() == ID_next_symbol)
-      dest.insert(src.get(ID_identifier));
-  }
+    const irept &c_sizeof_type = e.find(ID_C_c_sizeof_type);
 
-  const irept &c_sizeof_type=src.find(ID_C_c_sizeof_type);
+    if(c_sizeof_type.is_not_nil())
+      find_symbols(kind, static_cast<const typet &>(c_sizeof_type), dest);
 
-  if(c_sizeof_type.is_not_nil())
-    find_symbols(kind, static_cast<const typet &>(c_sizeof_type), dest);
+    const irept &va_arg_type = e.find(ID_C_va_arg_type);
 
-  const irept &va_arg_type=src.find(ID_C_va_arg_type);
-
-  if(va_arg_type.is_not_nil())
-    find_symbols(kind, static_cast<const typet &>(va_arg_type), dest);
+    if(va_arg_type.is_not_nil())
+      find_symbols(kind, static_cast<const typet &>(va_arg_type), dest);
+  });
 }
 
 void find_symbols(kindt kind, const typet &src, find_symbols_sett &dest)
@@ -124,9 +150,14 @@ void find_symbols(kindt kind, const typet &src, find_symbols_sett &dest)
     for(const typet &subtype : to_type_with_subtypes(src).subtypes())
       find_symbols(kind, subtype, dest);
 
-    const irep_idt &typedef_name=src.get(ID_C_typedef);
-    if(!typedef_name.empty())
-      dest.insert(typedef_name);
+    if(
+      kind == kindt::F_TYPE || kind == kindt::F_TYPE_NON_PTR ||
+      kind == kindt::F_ALL)
+    {
+      const irep_idt &typedef_name = src.get(ID_C_typedef);
+      if(!typedef_name.empty())
+        dest.insert(typedef_name);
+    }
   }
 
   if(src.id()==ID_struct ||
@@ -152,17 +183,22 @@ void find_symbols(kindt kind, const typet &src, find_symbols_sett &dest)
     // do the size -- the subtype is already done
     find_symbols(kind, to_array_type(src).size(), dest);
   }
-  else if(src.id()==ID_c_enum_tag)
+  else if(
+    kind == kindt::F_TYPE || kind == kindt::F_TYPE_NON_PTR ||
+    kind == kindt::F_ALL)
   {
-    dest.insert(to_c_enum_tag_type(src).get_identifier());
-  }
-  else if(src.id()==ID_struct_tag)
-  {
-    dest.insert(to_struct_tag_type(src).get_identifier());
-  }
-  else if(src.id()==ID_union_tag)
-  {
-    dest.insert(to_union_tag_type(src).get_identifier());
+    if(src.id() == ID_c_enum_tag)
+    {
+      dest.insert(to_c_enum_tag_type(src).get_identifier());
+    }
+    else if(src.id() == ID_struct_tag)
+    {
+      dest.insert(to_struct_tag_type(src).get_identifier());
+    }
+    else if(src.id() == ID_union_tag)
+    {
+      dest.insert(to_union_tag_type(src).get_identifier());
+    }
   }
 }
 
--- src/util/find_symbols.h
+++ src/util/find_symbols.h
@@ -50,24 +50,26 @@ find_symbols_sett find_symbols_or_nexts(iteratort begin, iteratort end)
   return result;
 }
 
-/// Add to the set \p dest the sub-expressions of \p src with id ID_symbol if
-/// \p current is true, and ID_next_symbol if \p next is true
-void find_symbols(
-  const exprt &src,
-  find_symbols_sett &dest,
-  bool current,
-  bool next);
-
 /// Find sub expressions with id ID_symbol
 void find_symbols(
   const exprt &src,
   std::set<symbol_exprt> &dest);
 
 /// Find sub expressions with id ID_symbol
-std::set<symbol_exprt> find_symbols(const exprt &src);
+inline std::set<symbol_exprt> find_symbols(const exprt &src)
+{
+  std::set<symbol_exprt> syms;
+  find_symbols(src, syms);
+  return syms;
+}
 
 /// Find identifiers of the sub expressions with id ID_symbol
-std::unordered_set<irep_idt> find_symbol_identifiers(const exprt &src);
+inline find_symbols_sett find_symbol_identifiers(const exprt &src)
+{
+  find_symbols_sett identifiers;
+  find_symbols(src, identifiers, true, false);
+  return identifiers;
+}
 
 DEPRECATED(SINCE(2022, 3, 14, "pick a specific implementation of find_symbols"))
 /// \return true if one of the symbols in \p src is present in \p symbols
