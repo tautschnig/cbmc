54a8782845e5c3d8ae3f01cf2762d69a1062d79c
Michael Tautschnig <tautschn@amazon.com>
2023-04-24 14:39:09 +0000

    Revert "Trying to get extensionality working"
    This reverts commit de10e0dc154547636affdfd9df87f87507ecb951.

--- regression/cbmc/Array_UF22/main.c
+++ regression/cbmc/Array_UF22/main.c
@@ -1,12 +1,5 @@
-int ag[__CPROVER_constant_infinity_uint];
-int bg[__CPROVER_constant_infinity_uint];
-
 int main()
 {
-  ag[0] = 1;
-  bg[0] = 1;
-  __CPROVER_assert(__CPROVER_array_equal(ag, bg), "equal");
-
   int a[2] = {0};
   int b[2] = {0};
   __CPROVER_assert(__CPROVER_array_equal(a, b), "equal");
